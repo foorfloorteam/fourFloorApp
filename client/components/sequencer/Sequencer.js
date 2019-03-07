63aff5186304e5d8c3c348790a18bdf81e7017bf
Kris Gui <gui.kris@gmail.com>
2019-03-06 22:13:15 -0600

    added kick button

--- client/components/sequencer/Sequencer.js
+++ client/components/sequencer/Sequencer.js
@@ -1,19 +1,45 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Howl, Howler} from 'howler'
-import ReactHowler from 'react-howler'
+import Tone from 'Tone'
+
+const synth = new Tone.MembraneSynth().toMaster()
+const sequence = ['C2', 'C2', 'C2', 'C2']
+const playSynth = () => synth.triggerAttackRelease('C2', '8n')
+const kickSequence = new Tone.Sequence(
+  (time, note) => playSynth(),
+  sequence,
+  '4n'
+)
 
 class Sequencer extends React.Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {sequence: ['C2', 'C2', 'C2', 'C2']}
+
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+  }
+
+  handleButtonClick(e) {
+    e.preventDefault()
+    this.kick.start(100, 5)
+  }
+
+  kick(time, note) {
+    const sequence1 = new Tone.Sequence(
+      (time, note) => playSynth(),
+      sequence,
+      '4n'
+    )
+    return sequence1
   }
 
   render() {
     return (
       <div className="container">
         <h1>Hello from Sequencer Component!</h1>
-        <ReactHowler src={['1-hihat1.wav']} preload={true} playing={true} />
+        <button type="button" onClick={this.handleButtonClick}>
+          playSynth
+        </button>
       </div>
     )
   }
