c46b94a17f86f2caf5d3d183cd25280240cdfe9f
Grant Weiss <grant.sweiss@gmail.com>
2019-03-07 14:21:44 -0600

    update state issues

--- client/components/instruments/Snare.js
+++ client/components/instruments/Snare.js
@@ -1,32 +1,94 @@
 import React from 'react'
 import {Card, Col, Row} from 'react-bootstrap'
+import Tone from 'Tone'
 
-export const Snare = props => {
-  const {snareArray, toggleActive, active, css} = props
-  return (
-    <div>
-      <Row>
-        <Col xs={2}>
-          <Card>
-            <Card.Title>Snare</Card.Title>
-          </Card>
-        </Col>
-        <Col xs={10}>
-          {snareArray
-            ? snareArray.map((cell, idx) => (
-                <div
-                  className={
-                    active.includes(idx + 16)
-                      ? `cell cell-color ${css}`
-                      : `cell cell-color`
-                  }
-                  onClick={() => toggleActive(idx + 16)}
-                  key={idx + 16}
-                />
-              ))
-            : 'no cell'}
-        </Col>
-      </Row>
-    </div>
-  )
+var player = new Tone.Player('../../1-snare1.wav').toMaster()
+
+export class Snare extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      snare: [],
+      active: [],
+      css: 'active'
+    }
+    this.pushSnareVal = this.pushSnareVal.bind(this)
+    this.toggleActive = this.toggleActive.bind(this)
+    this.handlePlay = this.handlePlay.bind(this)
+  }
+  makeRow() {
+    let row = []
+    for (let i = 0; i < 16; i++) {
+      row.push(null)
+    }
+    return row
+  }
+  componentDidMount() {
+    const row = this.makeRow()
+    this.setState({
+      snare: row
+    })
+  }
+  toggleActive(idx) {
+    this.setState({
+      active: this.state.active.includes(idx)
+        ? this.state.active.filter(item => item !== idx)
+        : [...this.state.active, idx]
+    })
+  }
+  pushSnareVal(idx) {
+    this.toggleActive(idx)
+    this.state.snare[idx] === null
+      ? (this.state.snare[idx] = 'C3')
+      : (this.state.snare[idx] = null)
+    this.toggleActive(idx)
+    console.log(idx)
+    console.log(this.state.snare)
+  }
+  handlePlay() {
+    const synth = new Tone.MembraneSynth().toMaster()
+    const synthPart = new Tone.Sequence(
+      function(time, note) {
+        synth.triggerAttackRelease(note, '10hz', time)
+      },
+      this.state.snare,
+      '16n'
+    )
+    synthPart.start()
+    Tone.Transport.start()
+  }
+  render() {
+    console.log(this.state)
+    const {snare} = this.state
+    return (
+      <div>
+        <Row>
+          <Col xs={2}>
+            <button type="button" onClick={this.handlePlay}>
+              playSynth
+            </button>
+            <Card>
+              <Card.Title>Snare</Card.Title>
+            </Card>
+          </Col>
+          <Col xs={10}>
+            {snare
+              ? snare.map((cell, idx) => (
+                  <div
+                    className={
+                      this.state.active.includes(idx)
+                        ? `cell cell-color ${this.state.css}`
+                        : `cell cell-color`
+                    }
+                    onClick={() => this.pushSnareVal(idx)}
+                    key={idx}
+                  />
+                ))
+              : 'no cell'}
+          </Col>
+        </Row>
+        <button onClick={() => player.start()}>test</button>
+      </div>
+    )
+  }
 }
--- client/components/sequencer/Sequencer.js
+++ client/components/sequencer/Sequencer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import Tone from 'Tone'
 import {Kick} from '../instruments/Kick'
 import {Snare} from '../instruments/Snare'
 import Transport from './Transport'
@@ -10,75 +9,18 @@ class Sequencer extends React.Component {
     super(synth)
     this.state = {
       active: [],
-      css: 'active',
-      kick: [],
-      snare: []
+      css: 'active'
     }
     this.notes = ['C3', 'Eb3', 'G3', 'Bb3']
-    this.handlePlay = this.handlePlay.bind(this)
-    this.toggleActive = this.toggleActive.bind(this)
-    this.pushKickVal = this.pushKickVal.bind(this)
-  }
-  makeRow() {
-    let row = []
-    for (let i = 0; i < 16; i++) {
-      row.push(null)
-    }
-    return row
-  }
-  componentDidMount() {
-    const row = this.makeRow()
-    this.setState({
-      kick: row,
-      snare: row
-    })
-  }
-  handlePlay() {
-    const synth = new Tone.MembraneSynth().toMaster()
-    const synthPart = new Tone.Sequence(
-      function(time, note) {
-        synth.triggerAttackRelease(note, '10hz', time)
-      },
-      this.state.kick,
-      '16n'
-    )
-    synthPart.start()
-    Tone.Transport.start()
-  }
-  toggleActive(idx) {
-    this.setState({
-      active: this.state.active.includes(idx)
-        ? this.state.active.filter(item => item !== idx)
-        : [...this.state.active, idx]
-    })
-  }
-  pushKickVal(idx) {
-    this.state.kick[idx] === null
-      ? (this.state.kick[idx] = 'C2')
-      : (this.state.kick[idx] = null)
-    this.toggleActive(idx)
   }
   render() {
     return (
       <div className="container">
         <h1>Hello from Sequencer Component!</h1>
         <Transport />
-        <button type="button" onClick={this.handlePlay}>
-          playSynth
-        </button>
+        <Kick />
 
-        <Kick
-          kickArray={this.state.kick}
-          active={this.state.active}
-          css={this.state.css}
-          pushKickVal={this.pushKickVal}
-        />
-
-        <Snare
-          snareArray={this.state.snare}
-          active={this.state.active}
-          css={this.state.css}
-        />
+        <Snare />
       </div>
     )
   }
