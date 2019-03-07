733d29260abb7d25303c24d41a5aac1809fe7bc1
Grant Weiss <grant.sweiss@gmail.com>
2019-03-07 00:19:11 -0600

    get test working

--- client/components/sequencer/Sequencer.js
+++ client/components/sequencer/Sequencer.js
@@ -2,42 +2,33 @@ import React from 'react'
 import {connect} from 'react-redux'
 import Tone from 'Tone'
 
-const synth = new Tone.MembraneSynth().toMaster()
-const sequence = ['C2', 'C2', 'C2', 'C2']
-const playSynth = () => synth.triggerAttackRelease('C2', '8n')
-const kickSequence = new Tone.Sequence(
-  (time, note) => playSynth(),
-  sequence,
-  '4n'
-)
-
 class Sequencer extends React.Component {
-  constructor() {
-    super()
-    this.state = {sequence: ['C2', 'C2', 'C2', 'C2']}
-
-    this.handleButtonClick = this.handleButtonClick.bind(this)
+  constructor(synth) {
+    super(synth)
+    this.state = {
+      grid: [],
+      sequence: ['C2', 'C2', 'C2', 'C2']
+    }
+    this.notes = ['C3', 'Eb3', 'G3', 'Bb3']
+    this.handlePlay = this.handlePlay.bind(this)
   }
-
-  handleButtonClick(e) {
-    e.preventDefault()
-    this.kick.start(100, 5)
-  }
-
-  kick(time, note) {
-    const sequence1 = new Tone.Sequence(
-      (time, note) => playSynth(),
-      sequence,
+  handlePlay(e) {
+    const synth = new Tone.MembraneSynth().toMaster()
+    const synthPart = new Tone.Sequence(
+      function(time, note) {
+        synth.triggerAttackRelease(note, '10hz', time)
+      },
+      this.state.sequence,
       '4n'
     )
-    return sequence1
+    synthPart.start()
+    Tone.Transport.start()
   }
-
   render() {
     return (
       <div className="container">
         <h1>Hello from Sequencer Component!</h1>
-        <button type="button" onClick={this.handleButtonClick}>
+        <button type="button" onClick={this.handlePlay}>
           playSynth
         </button>
       </div>
