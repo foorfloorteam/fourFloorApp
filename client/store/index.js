6180b5736773405dda84d58abcd357a397f431ac
Grant Weiss <grant.sweiss@gmail.com>
2021-04-21 15:35:00 -0500

    travis

--- client/store/index.js
+++ client/store/index.js
@@ -4,9 +4,7 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 
 const reducer = combineReducers({user})
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware)
-)
+const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware))
 const store = createStore(reducer, middleware)
 
 export default store
